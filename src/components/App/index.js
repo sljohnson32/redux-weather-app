4c0cadcf402541ec907833587f0f4f572520a103
Rebekah Lundy <lundyrebekah@gmail.com>
2017-02-02 17:45:54 -0700

    Revert "fix"

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './app-style.css'
-import moment from 'moment';
 // import { connect } from 'react-redux';
 
 import Header from '../Header';
@@ -28,8 +27,7 @@ export default class App extends Component {
   fetchSunAPI(lat, long, fetchSun) {
     fetch(`http://api.sunrise-sunset.org/json?lat=${lat}&lng=${long}`)
     .then((response) => response.json())
-    .then((data) => this.filterTime(data))
-    .then((cleanData) => fetchSun(cleanData))
+    .then((data) => fetchSun(data))
   }
 
   //MOVE TO HELPER FUNCTION
@@ -39,47 +37,11 @@ export default class App extends Component {
     let state = array[1].toString().split(' ').join('_');
     return `${state}/${city}`;
   }
-
-  filterData(data){
-    return {
-      city: data.current_observation.display_location.city ,
-      state: data.current_observation.display_location.state,
-      fullName: data.current_observation.display_location.full,
-      localTime: data.current_observation.local_time_rfc822,
-      weather: data.current_observation.weather,
-      temp_f: data.current_observation.temp_f,
-      temp_c: data.current_observation.temp_c,
-      humidity: data.current_observation.relative_humidity,
-      wind_string: data.current_observation.wind_string,
-      wind_dir: data.current_observation.wind_dir,
-      wind_mph: data.current_observation.wind_mph,
-      icon_url: data.current_observation.icon_url
-    }
-  }
-
-  filterTime(data){
-    return{
-      sunrise: data.results.sunrise,
-      sunset: data.results.sunset
-    }
-  }
-
   getForecast(location, func) {
     fetch(`http://api.wunderground.com/api/0b7e4bc2937ad616/conditions/q/${this.splitLocation(location)}.json`)
     .then((response) => response.json())
-    .then((data) => this.filterData(data))
-    .then((cleanData) => func(cleanData))
-    // .then((data) => this.checkD aytime(data))
+    .then((data) => func(data))
   }
-
-  // checkDaytime(data) {
-  //   console.log(props.data.results);
-  //   let sunrise = moment.parseZone(`${this.props.sunrise.sunrise}`).local().format('HH:mm:ss')
-  //    let now = moment().format('HH:mm:ss')
-  //    let sunset = moment.parseZone(`${this.props.sunrise.sunset}`).local().format('HH:mm:ss')
-  //    if (now > sunrise && now < sunset){console.log("It's fuckin' daytime, baby!")}
-  //    console.log(sunrise, sunset);
-  //   }
 
   render() {
     return (
