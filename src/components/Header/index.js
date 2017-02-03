6eab21bfef1c446e8e969b7a68768b5f70b307d8
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-02-02 22:31:57 -0700

    change loading phrase, add icon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,17 +3,16 @@ import React from 'react';
 import './header-style.css';
 
 const Header = (props) => {
-  // debugger
-  console.log('props-lat&long', props.data.results)
   return (
     <div className='header'>
       <h1>Weather</h1>
       <div className='header-container'>
         {props.data.city  ?
           <div className='header-text-container'>
+            <img className='icon' src={props.data.icon_url} />
           <p className='current'>Current temperature for <p className='city'>{props.data.city}: {props.data.temp_f}&#176;F</p> </p>
         </div>
-           : <p>not loaded yet!</p>}
+           : <p>Please be patient while we get your weather!</p>}
       </div>
 
 
