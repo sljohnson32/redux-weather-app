46cb054666202141fe65462694f6cf458b3d4047
bekahlundy <lundyrebekah@gmail.com>
2017-02-03 16:57:39 -0700

    all cards render what is in the settings with city, temp, and skies

--- src/components/PinBox/index.js
+++ src/components/PinBox/index.js
@@ -11,9 +11,14 @@ export default class PinBox extends Component {
         <p className='pinbox-title'>Favorites</p>
         <div className='card-container'>
               <div className='one'>{this.props.PinForecastData.length === 0 ? <p>loading........</p> : <div><p>{this.props.PinForecastData[0].fullName}</p>
-              <p>{this.props.PinForecastData[0].temp_f}</p></div>}</div>
-              <div className='two'></div>
-              <div className='three'></div>
+              <p>{this.props.PinForecastData[0].temp_f}</p>
+              <p>{this.props.PinForecastData[0].weather}</p></div>}</div>
+              <div className='two'>{this.props.PinForecastData.length === 0 ? <p>loading........</p> : <div><p>{this.props.PinForecastData[1].fullName}</p>
+              <p>{this.props.PinForecastData[1].temp_f}</p>
+              <p>{this.props.PinForecastData[1].weather}</p></div>}</div>
+              <div className='three'>{this.props.PinForecastData.length === 0 ? <p>loading........</p> : <div><p>{this.props.PinForecastData[2].fullName}</p>
+              <p>{this.props.PinForecastData[2].temp_f}</p>
+              <p>{this.props.PinForecastData[2].weather}</p></div>}</div>
 
           <button className='edit-btn'><Link to='/settings'>Edit Pinned Cities</Link></button>
         </div>
