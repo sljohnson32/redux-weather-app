40d4faf463297387b056d2e371d3ccaf98cfe58b
bekahlundy <lundyrebekah@gmail.com>
2017-02-03 16:51:04 -0700

    city and temp showing on cards yayay

--- src/components/PinBox/index.js
+++ src/components/PinBox/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
 import './pinbox-style.css'
 // import { connect } from 'react-redux';
+export default class PinBox extends Component {
+  render() {
+    console.log('pinbox-props', this.props.PinForecastData[0])
+    // debugger
+    return (
+      <div className='pinbox-container'>
+        <p className='pinbox-title'>Favorites</p>
+        <div className='card-container'>
+              <div className='one'>{this.props.PinForecastData.length === 0 ? <p>loading........</p> : <div><p>{this.props.PinForecastData[0].fullName}</p>
+              <p>{this.props.PinForecastData[0].temp_f}</p></div>}</div>
+              <div className='two'></div>
+              <div className='three'></div>
 
-const PinBox = () => {
-  return (
-    <div className='pinbox-container'>
-      <p className='pinbox-title'>Favorites</p>
-      <div className='card-container'>
-        <div className='one'></div>
-        <div className='two'></div>
-        <div className='three'></div>
-        <button className='edit-btn'><Link to='/settings'>Edit Pinned Cities</Link></button>
+          <button className='edit-btn'><Link to='/settings'>Edit Pinned Cities</Link></button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
-export default PinBox;
