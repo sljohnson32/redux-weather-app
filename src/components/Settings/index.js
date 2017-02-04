fdeacdd99b75213f9867eea430ef05a14fa00362
sljohnson32 <sljohnson32@gmail.com>
2017-02-04 11:27:17 -0700

    fix merge

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -16,31 +16,12 @@ export default class Settings extends Component {
     const { cityInput } = this.state;
     return (
       <div>
-<<<<<<< HEAD
         <p className='settings-p-tag'>Settings</p>
         <div className='container'>
           {data.map((city, i) => {
             return <SettingsCity key={ i } pinID={i} data={city} removePin={removePin} />
           })}
           <AddPin data={data} receiveForecast={receiveForecast} />
-=======
-        {data.fullName}
-        {data.map((city, i) => {
-          return <SettingsCity key={ i } pinID={i} data={city} removePin={removePin} />
-        })}
-        <p className='settings-p-tag'>Settings</p>
-        <div className='container'>
-          <input
-            value={this.state.cityInput}
-            onChange={ (e)=> this.setState({ cityInput: e.target.value })}
-            placeholder='Enter a city'
-          />
-          <button
-            className='search-btn'
-            disabled={disableBtn}
-            onClick={ () => this.getForecast(cityInput, receiveForecast) }
-          >Add New City</button>
->>>>>>> master
         </div>
       </div>
     )
