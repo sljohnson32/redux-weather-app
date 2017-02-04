c01c238eeaab6147b24784d7a5a53444666f6a15
sljohnson32 <sljohnson32@gmail.com>
2017-02-04 10:56:39 -0700

    add helpers to Settings component

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SettingsCity from '../SettingsCity'
+const { splitLocation, filterData } = require('../Helpers/ForecastHelpers');
 import './settings-style.css'
 
 export default class Settings extends Component {
@@ -10,34 +11,10 @@ export default class Settings extends Component {
     }
   }
 
-  splitLocation(location) {
-    let array = location.split(',');
-    let city = array[0];
-    let state = array[1].toString().split(' ').join('_');
-    return `${state}/${city}`;
-  }
-
-  filterData(data) {
-    return {
-      city: data.current_observation.display_location.city ,
-      state: data.current_observation.display_location.state,
-      fullName: data.current_observation.display_location.full,
-      localTime: data.current_observation.local_time_rfc822,
-      weather: data.current_observation.weather,
-      temp_f: data.current_observation.temp_f,
-      temp_c: data.current_observation.temp_c,
-      humidity: data.current_observation.relative_humidity,
-      wind_string: data.current_observation.wind_string,
-      wind_dir: data.current_observation.wind_dir,
-      wind_mph: data.current_observation.wind_mph,
-      icon_url: data.current_observation.icon_url
-    }
-  }
-
   getForecast(location, func) {
-    fetch(`http://api.wunderground.com/api/0b7e4bc2937ad616/conditions/q/${this.splitLocation(location)}.json`)
+    fetch(`http://api.wunderground.com/api/0b7e4bc2937ad616/conditions/q/${splitLocation(location)}.json`)
     .then((response) => response.json())
-    .then((data) => this.filterData(data))
+    .then((data) => filterData(data))
     .then((cleanData) => func(cleanData))
   }
 
