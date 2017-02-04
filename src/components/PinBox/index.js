767e3d937e9f2585e84312cf75a63520af0d1033
bekahlundy <lundyrebekah@gmail.com>
2017-02-03 17:05:31 -0700

    refactor data on pinbox page

--- src/components/PinBox/index.js
+++ src/components/PinBox/index.js
@@ -10,16 +10,30 @@ export default class PinBox extends Component {
       <div className='pinbox-container'>
         <p className='pinbox-title'>Favorites</p>
         <div className='card-container'>
-              <div className='one'>{this.props.PinForecastData.length === 0 ? <p>loading........</p> : <div><p>{this.props.PinForecastData[0].fullName}</p>
-              <p>{this.props.PinForecastData[0].temp_f}</p>
-              <p>{this.props.PinForecastData[0].weather}</p></div>}</div>
-              <div className='two'>{this.props.PinForecastData.length === 0 ? <p>loading........</p> : <div><p>{this.props.PinForecastData[1].fullName}</p>
+          <div className='one'>
+            {this.props.PinForecastData.length === 0 ? <p>add a city</p> :
+              <div>
+                <p>{this.props.PinForecastData[0].fullName}</p>
+                <p>{this.props.PinForecastData[0].temp_f}</p>
+                <p>{this.props.PinForecastData[0].weather}</p>
+          </div>}
+        </div>
+        <div className='two'>
+          {this.props.PinForecastData.length === 0 ? <p>add a city</p> :       
+            <div>
+              <p>{this.props.PinForecastData[1].fullName}</p>
               <p>{this.props.PinForecastData[1].temp_f}</p>
-              <p>{this.props.PinForecastData[1].weather}</p></div>}</div>
-              <div className='three'>{this.props.PinForecastData.length === 0 ? <p>loading........</p> : <div><p>{this.props.PinForecastData[2].fullName}</p>
+              <p>{this.props.PinForecastData[1].weather}</p>
+            </div>}
+        </div>
+        <div className='three'>
+          {this.props.PinForecastData.length === 0 ? <p>add a city</p> :
+            <div>
+              <p>{this.props.PinForecastData[2].fullName}</p>
               <p>{this.props.PinForecastData[2].temp_f}</p>
-              <p>{this.props.PinForecastData[2].weather}</p></div>}</div>
-
+              <p>{this.props.PinForecastData[2].weather}</p>
+            </div>}
+        </div>
           <button className='edit-btn'><Link to='/settings'>Edit Pinned Cities</Link></button>
         </div>
       </div>
