2fb0cab6bee23b37ffef9572bee8fa4a92be0281
bekahlundy <lundyrebekah@gmail.com>
2017-02-05 09:59:52 -0700

    pushing up broken reducer testing  code so sam can take a look at it

--- test/reducers/app.test.js
+++ test/reducers/app.test.js
@@ -1,29 +1,48 @@
 import reducer from '../../src/reducers/settings-reducer'
 import settings from '../../src/reducers/settings-reducer'
 
+
 describe('reducers', () => {
   it('should return the initial state', () => {
     expect( reducer(undefined, {}) ).toEqual([])
   })
 })
 
-describe('settings reducer', function () {
+describe('settings reducer', () => {
 
     it('should return an initial state', () => {
       expect(settings(undefined, {})).toEqual([]);
     });
 
-    it('should accept action RECEIVE_FORECAST', () => {
-      expect(
-        reducer({}, {
-          type: 'RECEIVE_FORECAST',
-          forecastData: {}
-        })
-      ).toEqual(
-        [{}]
-      )
-
-    })
+    // it.skip('should accept action RECEIVE_FORECAST', () => {
+    //   this.action = {
+    //     type: 'RECEIVE_FORECAST',
+    //     data: {
+    //         forecastData: []
+    //     },
+    //   };
+    //   expect(settings(this.action.data)).toEqual(this.action.data);
+    // });
+    //
+    // it.skip('should accept action RECEIVE_FORECAST', () => {
+    //   expect(
+    //     reducer([], {
+    //       type: 'RECEIVE_FORECAST',
+    //       forecastData: {}
+    //     })
+    //   ).toEqual([
+    //     {}
+    //   ])
+    // })
+    //
+    // it.skip('should accept action REMOVE_PIN', () => {
+    //   expect(
+    //     reducer([], {
+    //       type: 'RECEIVE_PIN',
+    //       pinID: 0
+    //     })
+    //   ).toEqual([])
+    // })
   })
 
   describe('app reducer', function () {
