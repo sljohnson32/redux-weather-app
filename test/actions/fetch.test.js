a1292e50f326b93605ee47ccb763bb0601a61153
bekahlundy <lundyrebekah@gmail.com>
2017-02-04 18:57:42 -0700

    finished action tests

--- test/actions/fetch.test.js
+++ test/actions/fetch.test.js
@@ -9,4 +9,36 @@ describe('actions', () => {
     }
     expect(actions.receiveForecast(forecastData)).toEqual(expectedAction)
   })
+  it('should create an action to fetch location', () => {
+    const data = {}
+    const expectedAction = {
+     type: 'FETCH_LOCATION',
+     data: {}
+    }
+    expect(actions.fetchLocation(data)).toEqual(expectedAction)
+  })
+  it('should create an action to fetch sun', () => {
+    const time = {}
+    const expectedAction = {
+     type: 'FETCH_SUN',
+     time: {}
+    }
+    expect(actions.fetchSun(time)).toEqual(expectedAction)
+  })
+  it('should create an action to fetch weather', () => {
+    const location = []
+    const expectedAction = {
+     type: 'FETCH_WEATHER',
+     location: []
+    }
+    expect(actions.fetchWeather(location)).toEqual(expectedAction)
+  })
+  it('should create an action to remove pin', () => {
+    const pinID = []
+    const expectedAction = {
+     type: 'REMOVE_PIN',
+     pinID: []
+    }
+    expect(actions.removePin(pinID)).toEqual(expectedAction)
+  })
 })
