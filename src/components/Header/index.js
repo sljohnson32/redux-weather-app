42fa167b7f0fa3fe7fa508fcd25262eb517f2d06
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-02-05 12:01:38 -0700

    add time of days to header container

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,32 +4,37 @@ import moment from 'moment';
 import './header-style.css';
 
 const Header = (props) => {
-  // console.log(props);
-  // let now = moment().format('HH:mm:ss')
-  // let sunrise = moment.parseZone(`${props.sunrise}`).local().format('HH:mm:ss')
-  // let morning = moment(sunrise, 'HH:mm:ss').add(2, 'h').format('HH:mm:ss')
-  // let sunset = moment.parseZone(`${props.sunset}`).local().format('HH:mm:ss')
-  // let evening = moment(sunset, 'HH:mm:ss').subtract(2, 'h').format('HH:mm:ss')
-  // switch (true) {
-  //   case (sunrise < now && now < morning):
-  //     message = "Wakey wakey!"
-  //     break;
-  //   case (morning < now && now < evening):
-  //     message = "Work time!"
-  //     break;
-  //   case (evening < now && now < sunset):
-  //     message = "Have you eaten yet?"
-  //     break;
-  //   default:
-  //     message = "Maybe go to bed"
-  //
-  // }
-  // console.log(sunrise, morning, evening, sunset);
+  console.log(props);
+  let message = ''
+  let now = moment().format('HH:mm:ss')
+  let sunrise = moment.parseZone(`${props.data.sunrise}`).local().format('HH:mm:ss')
+  let morning = moment(sunrise, 'HH:mm:ss').add(2, 'h').format('HH:mm:ss')
+  let sunset = moment.parseZone(`${props.data.sunset}`).local().format('HH:mm:ss')
+  let evening = moment(sunset, 'HH:mm:ss').subtract(2, 'h').format('HH:mm:ss')
+  switch (true) {
+    case (sunrise < now && now < morning):
+      message = "Wakey wakey!"
+      break;
+    case (morning < now && now < evening):
+      message = "Work time!"
+      break;
+    case (evening < now && now < sunset):
+      message = "Have you eaten yet?"
+      break;
+    default:
+      message = "Maybe go to bed"
+
+  }
+  console.log(sunrise, morning, evening, sunset);
 
   return (
     <div className='header'>
       <h1>Weather</h1>
       <div className='header-container'>
+        <section className='time-container'>
+          sunrise: {sunrise}, sunset: {sunset}, current time: {now}
+          <br/>{message}
+        </section>
         {props.data.city  ?
           <div className='header-text-container'>
           <p className='current'>Current temperature for <span className='city'>{props.data.city}: {props.data.temp_f}&#176;F</span> </p>
