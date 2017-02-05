480f7c5dc15cd55199519147a85b576aae836f1d
bekahlundy <lundyrebekah@gmail.com>
2017-02-04 18:51:01 -0700

    first test working, recieve forecast

--- test/actions/fetch.test.js
+++ test/actions/fetch.test.js
@@ -1,7 +1,12 @@
 import * as actions from '../../src/actions'
 
 describe('actions', () => {
-  it('should create an action to add a todo', () => {
-    expect(true).toEqual(true)
+  it('should create an action to remove pin', () => {
+    const forecastData = []
+    const expectedAction = {
+     type: 'RECEIVE_FORECAST',
+     forecastData: []
+    }
+    expect(actions.receiveForecast(forecastData)).toEqual(expectedAction)
   })
 })
