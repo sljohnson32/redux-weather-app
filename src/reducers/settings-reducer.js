0450d101a94dadf90a3331a89568b7167070931c
bekahlundy <lundyrebekah@gmail.com>
2017-02-04 21:02:58 -0700

    second reducer test is working

--- src/reducers/settings-reducer.js
+++ src/reducers/settings-reducer.js
@@ -1,12 +1,12 @@
 const settings = (state = [], action) => {
   switch ('settings action', action.type) {
     case 'RECEIVE_FORECAST':
-      return [...state, action.forecastData]
+    return [...state, action.forecastData]
     case 'REMOVE_PIN':
-      const indexNum = action.pinID;
-      return [...state.slice(0, indexNum), ...state.slice(indexNum + 1)]
+    const indexNum = action.pinID;
+    return [...state.slice(0, indexNum), ...state.slice(indexNum + 1)]
     default:
-      return state;
+    return state;
   }
 }
 
--- test/reducers/app.test.js
+++ test/reducers/app.test.js
@@ -1,7 +1,30 @@
 import reducer from '../../src/reducers/settings-reducer'
+import settings from '../../src/reducers/settings-reducer'
 
-describe('todos reducer', () => {
+describe('reducers', () => {
   it('should return the initial state', () => {
     expect( reducer(undefined, {}) ).toEqual([])
   })
 })
+
+describe('settings reducer', function () {
+
+    it('should return an initial state', () => {
+      expect(settings(undefined, {})).toEqual([]);
+    });
+
+    // it('should accept action SET_ACTIVE_USER', () => {
+    //   this.action = {
+    //     type: 'SET_ACTIVE_USER',
+    //     data: {
+    //       id: 1,
+    //       name: 'Taylor',
+    //       password: 'password',
+    //       email: 'tman2272@aol.com',
+    //     },
+    //   };
+    //
+    //   const newUserState = user(this.state, this.action);
+    //   expect(newUserState).to.deep.equal(this.action.data);
+    // });
+  });
