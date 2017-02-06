e6d82279991af9e02820b596f3f353192e65a170
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-02-06 12:57:47 -0700

    rework AddPin tests, still failing with opening tag

--- test/components/addpin.test.js
+++ test/components/addpin.test.js
@@ -1,39 +1,36 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import AddPin from '../../src/components/AddPin';
+import { Provider } from 'react-redux'
 const { splitLocation, filterData } = require('../Helpers/ForecastHelpers');
 
 const setup = () => {
   const props = {
     handleClick: jest.fn(),
   }
-  const wrapper = mount(
-    <AddPin handleClick={props.getForecast}/>
-  )
 
-  const Component = wrapper.find(AddPin)
+  const wrapper = mount(
+    <AddPin handleClick={props.getForecast}/>);
+
+  const Component = wrapper.find(AddPin);
 
   return {
     props,
     Component
-  }
+  };
 }
 
 describe('AddPin', () => {
 
-  it('should render something', () => {
+  it('should render an input field', () => {
     const { Component } = setup();
-    expect(Component.find('input')).toEqual(1);
+    expect(Component.find(input)).toEqual(1);
   });
 
-  // it('should render an input field', () => {
-  //   const { Component } = setup();
-  //   expect(Component.find(input)).toEqual(1);
-  // });
-
   it('should simulate a button click', () => {
-    const wrapper = shallow(<div onButtonClick={onButtonClick} />);
-    wrapper.find('button').simulate('click');
-    expect(onButtonClick.calledOnce).toEqual(true);
-  })
+    const { Component } = setup();
+    Component.find('button').simulate('click');
+    expect(getForecast.calledOnce).toEqual(true);
+  });
+
 })
