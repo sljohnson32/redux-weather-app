883f3e1c932a7d233863ba98e16ddd963d238406
bekahlundy <lundyrebekah@gmail.com>
2017-02-05 21:58:23 -0700

    everything is now stored in local storage and stays on page refresh

--- src/index.js
+++ src/index.js
@@ -6,10 +6,23 @@ import { Router, browserHistory } from 'react-router';
 import Routes from './routes';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
- const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+import { loadState, saveState } from './localStorage';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 
 const middleware = [thunk];
-const store = createStore(rootReducer, {}, composeEnhancers(applyMiddleware(...middleware)))
+
+const persistedState = loadState();
+
+const store = createStore(
+  rootReducer,
+  persistedState,
+  composeEnhancers(applyMiddleware(...middleware))
+)
+
+store.subscribe(()=>{
+  saveState(store.getState());
+})
 
 render(
   <Provider store={store}>
