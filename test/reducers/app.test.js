9349921e00a8df8bf45049c5a044c08146293ae8
bekahlundy <lundyrebekah@gmail.com>
2017-02-05 18:11:45 -0700

    finished both settings and app reducer tests

--- test/reducers/app.test.js
+++ test/reducers/app.test.js
@@ -3,11 +3,13 @@ import settings from '../../src/reducers/settings-reducer'
 import app from '../../src/reducers/app-reducer'
 
 describe('todos reducer', () => {
+
   it('should return the initial state', () => {
     expect( reducer(undefined, {}) ).toEqual([])
   })
 
 describe('settings reducer', () => {
+
   it('should accept action RECEIVE_FORECAST', () => {
       const action = {
         type: 'RECEIVE_FORECAST',
@@ -15,6 +17,7 @@ describe('settings reducer', () => {
       };
       expect(settings([], action)).toEqual(['Sam is good at testing']);
     });
+
     it('should accept action RECIEVE_EXTENDED', () => {
         const action = {
           type: 'RECEIVE_EXTENDED',
@@ -22,6 +25,7 @@ describe('settings reducer', () => {
         };
         expect(settings([], action)).toEqual([]);
     });
+
     it('should accept action REMOVE_PIN', () => {
         const action = {
           type: 'REMOVE_PIN',
@@ -30,30 +34,35 @@ describe('settings reducer', () => {
         expect(settings([], action)).toEqual([]);
     });
 })
+
 describe('app reducer', () => {
+
   it('should return the initial state', ()=> {
-  expect(reducer(undefined, [])).toEqual([])
+    expect(reducer(undefined, [])).toEqual([])
   })
-  it.skip('should accept action FETCH_LOCATION', () => {
+
+  it('should accept action FETCH_LOCATION', ()=> {
+    const action =  {
+      type:"FETCH_LOCATION",
+      data:'fake data'
+    }
+  expect(app([], action)).toEqual('fake data')
+  })
+
+  it('should accept action FETCH_SUN', () => {
       const action = {
-        type: 'FETCH_LOCATION',
-        forecastData: 'Sam is good at testing'
+        type: 'FETCH_SUN',
+        time: [{1:1},{2:2},{3:3}]
       };
-      expect(settings([], action)).toEqual(['Sam is good at testing']);
+      expect(app([], action)).toEqual([{1:1},{2:2},{3:3}]);
     });
-    it.skip('should accept action RECIEVE_EXTENDED', () => {
+
+    it('should accept action FETCH_WEATHER', () => {
         const action = {
-          type: 'RECEIVE_EXTENDED',
-          extForecast: []
+          type: 'FETCH_WEATHER',
+          location: 'White Bear Lake'
         };
-        expect(settings([], action)).toEqual([]);
-    });
-    it.skip('should accept action REMOVE_PIN', () => {
-        const action = {
-          type: 'REMOVE_PIN',
-          pinID: []
-        };
-        expect(settings([], action)).toEqual([]);
+        expect(app([], action)).toEqual('White Bear Lake');
     });
 })
 })
