ec7cd35d0151e21506b0ceb7d072d0e64807ad90
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-02-06 09:11:45 -0700

    begin testing

--- test/components/addpin.test.js
+++ test/components/addpin.test.js
@@ -1,6 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
 import AddPin from '../../src/components/AddPin';
 const { splitLocation, filterData } = require('../Helpers/ForecastHelpers');
 
 describe('AddPin', () => {
-  it('should ')
+  it('should render an input field', () => {
+    const wrapper = shallow(<AddPin />);
+    expect(wrapper.find(input)).toEqual(1);
+  });
+
+  it('should simulate a button click', () => {
+    const wrapper = shallow(<div onButtonClick={onButtonClick} />);
+    wrapper.find('button').simulate('click');
+    expect(onButtonClick.calledOnce).toEqual(true);
+  })
 })
